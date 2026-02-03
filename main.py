6d65fb4b7583069e0c993f9f97c6ca9f03b1ad85
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2026-02-03 12:29:21 +0100

    Begin cleaning

--- main.py
+++ main.py
@@ -12,3 +12,22 @@ if __name__ == "__main__":
     # Print the number of rows and columns
     print(f"Number of rows: {rawData.shape[0]}")
     print(f"Number of columns: {rawData.shape[1]}")
+
+    cleanedData = pandas.DataFrame()
+
+    # Find 0 and 1 columns with no missing values
+    for column in rawData.columns:
+        if not rawData[column].isnull().any() and set(
+            rawData[column].dropna().unique()
+        ).issubset({0, 1}):
+            cleanedData[column] = rawData[column]
+            del rawData[column]
+
+    # Print the number of columns
+    print(f"Number of cleaned columns: {cleanedData.shape[1]}")
+    print(f"Number of remaining columns: {rawData.shape[1]}")
+
+    # Save the cleaned data
+    cleanedData.to_csv(
+        os.path.join(dataFolder, "mental_healthCleaned.csv"), index=False
+    )
