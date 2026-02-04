a89facb39bfdfefaef732f60a7f1ce9ff0a3b680
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2026-02-04 13:40:19 +0100

    Add correlation file

---
+++ correlation.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def calculateCorrelations(data: pd.DataFrame) -> dict:
+    pass
+
+
+def indexCorrelation() -> None:
+    """
+
+    Args:
+        - None
+
+    Returns:
+        - None
+    """
+    pass
+
+
+if __name__ == "__main__":
+    indexCorrelation()
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import dataCleaner
+import correlation
 
 if __name__ == "__main__":
 
     # Data Preparation & Quality Assurance
     dataCleaner.cleanData()
+
+    # Feature Engineering & Selection
+    correlation.indexCorrelation()
